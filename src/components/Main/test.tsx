177ced6ee58e805e81a1d4a6c08d8abec2994e7c
itallogravina <itallo.gravina@gmail.com>
2022-07-18 20:12:20 -0300

    erro test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,16 +6,8 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /MAin/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
-  })
-
-  it('should render the colors correctly', () => {
-    const { container } = render(<Main />)
-
-    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
